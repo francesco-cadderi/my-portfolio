f46fa0aa8959aa43aaab07f8e836f76d6e8ff674
Insowers <francesco.cadderi@gmail.com.com>
2024-01-14 18:48:41 +0100

    fix animation

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useRef } from "react";
-import { motion, useInView, useAnimation } from "framer-motion";
+import React from "react";
+import { motion } from "framer-motion";
 import "../index.css";
 
 function Block({ children }) {
-  const ref = useRef(null);
-  const isInView = useInView(ref);
-  const triggerAnimation = useAnimation();
-
-  useEffect(() => {
-    if (isInView) {
-      triggerAnimation.start("visible");
-      console.log("visible");
-    } else {
-        triggerAnimation.start("hidden");
-        console.log("hidden");
-    }
-  }, [isInView]);
-
   return (
-    <div ref={ref} className="container">
+    <div className="container">
       <motion.div
-        variants={{
-          hidden: { opacity: 0, y: 75 },
-          visible: { opacity: 1, y: 0 },
-        }}
-        initial="hidden"
-        animate={triggerAnimation}
-        transition={{ duration: 0.5, delay: 0.3}}
-        
+        initial={{ opacity: 0, y: 75 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
       >
         {children}
       </motion.div>
